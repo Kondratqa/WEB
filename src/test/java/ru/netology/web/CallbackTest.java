3e132050d2459cf4c0aaeb3e7b1640a870b58254
Kinomagnum <kinomagnum@gmail.com>
2024-08-20 18:01:30 +0300

    initialCommitReload

--- src/test/java/ru/netology/web/CallbackTest.java
+++ src/test/java/ru/netology/web/CallbackTest.java
@@ -8,8 +8,6 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
-import static com.codeborne.selenide.Selenide.open;
-
 public class CallbackTest {
     private WebDriver driver;
 
